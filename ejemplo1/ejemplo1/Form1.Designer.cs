801e88c60b815d47e8358d01c5d7ef6101a49dfa
ArielTapia <arieltapia42@gmail.com>
2024-04-08 19:28:54 -0300

    Actualizacion

---
+++ ejemplo1/ejemplo1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.cbLista = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Location = new System.Drawing.Point(78, 164);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(72, 13);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "Elija la opcion";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(334, 164);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(183, 274);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(0, 13);
+            this.lblMensaje.TabIndex = 3;
+            // 
+            // cbLista
+            // 
+            this.cbLista.FormattingEnabled = true;
+            this.cbLista.Items.AddRange(new object[] {
+            "Rojo",
+            "Verde",
+            "Azul"});
+            this.cbLista.Location = new System.Drawing.Point(177, 164);
+            this.cbLista.Name = "cbLista";
+            this.cbLista.Size = new System.Drawing.Size(120, 94);
+            this.cbLista.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbLista);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.CheckedListBox cbLista;
+    }
+}
+
